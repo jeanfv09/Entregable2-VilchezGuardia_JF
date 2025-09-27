e256c696f769c1fc8f364390244be734b90b3272
jeanfv09 <jeanfrancov09@gmail.com>
2025-09-26 20:07:49 -0500

    Iniciando Proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Entregable2_VilchezGuardia_JF.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
